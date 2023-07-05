2f84388f596618fc380577c7c95a61ff9f9851fa
Vladimir Davydov <v.g.davydoff@gmail.com>
2023-07-05 18:18:05 +0300

    Checked and delete

--- src/components/task-list.js
+++ src/components/task-list.js
@@ -1,26 +1,78 @@
-import React from "react";
+import React, { Component } from "react";
 import { Task } from "./task";
 
-export const TaskList = ({ todos }) => {
-  const elements = todos.map((elem) => {
-    let styleName = null;
-    if (elem.status === "completed") {
-      styleName = "completed";
-    }
-    if (elem.status === "editing") {
-      styleName = "editing";
+export class TaskList extends Component {
+  state = {
+    todoData: [
+      {
+        label: "Completed task",
+        time: "created 17 seconds ago",
+        status: null,
+        id: 1,
+      },
+      {
+        label: "Editing task",
+        time: "created 5 minutes ago",
+        status: null,
+        id: 2,
+      },
+      {
+        label: "Active task",
+        time: "created 5 minutes ago",
+        status: null,
+        id: 3,
+      },
+    ],
+  };
+  onTaskClick = (id) => {
+    this.setState(({ todoData }) => {
+      const newArr = JSON.parse(JSON.stringify(todoData));
+      newArr.map((elem) => {
+        if (elem.id === id && elem.status === null) {
+          elem.status = "completed";
+        } else if (elem.id === id && elem.status === "completed") {
+          elem.status = null;
+        }
+      });
+      return {
+        todoData: newArr,
+      };
+    });
+  };
+  onTaskDelete = (id) => {
+    this.setState(({ todoData }) => {
+      const idx = todoData.findIndex((elem) => elem.id === id);
+      const newArr = [...todoData.slice(0, idx), ...todoData.slice(idx + 1)];
+      return {
+        todoData: newArr,
+      };
+    });
+  };
+  render() {
+    const elements = this.state.todoData.map((elem) => {
+      let styleName = elem.status;
+      if (elem.status === "completed") {
+        styleName = "completed";
+      }
+      if (elem.status === "editing") {
+        styleName = "editing";
+        return (
+          <li className={styleName} key={elem.id}>
+            <input className="edit" type="text" value={elem.label}></input>
+          </li>
+        );
+      }
       return (
         <li className={styleName} key={elem.id}>
-          <Task label={elem.label} time={elem.time} />
-          <input className="edit" type="text" value="Editing task"></input>
+          <Task
+            label={elem.label}
+            time={elem.time}
+            onCheked={() => this.onTaskClick(elem.id)}
+            onDeleted={() => this.onTaskDelete(elem.id)}
+          />
         </li>
       );
-    }
-    return (
-      <li className={styleName} key={elem.id}>
-        <Task label={elem.label} time={elem.time} />
-      </li>
-    );
-  });
-  return <ul className="todo-list">{elements}</ul>;
-};
+    });
+    return <ul className="todo-list">{elements}</ul>;
+  }
+}
--- src/components/task.js
+++ src/components/task.js
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
 
-export const Task = (props) => {
-  return (
-    <div className="view">
-      <input className="toggle" type="checkbox"></input>
-      <label>
-        <span className="description">{props.label}</span>
-        <span className="created">{props.time}</span>
-      </label>
-      <button className="icon icon-edit"></button>
-      <button className="icon icon-destroy"></button>
-    </div>
-  );
-};
+export class Task extends Component {
+  render() {
+    const { label, time, onCheked, onDeleted } = this.props;
+    return (
+      <div className="view">
+        <input className="toggle" type="checkbox" onChange={onCheked}></input>
+        <label>
+          <span className="description">{label}</span>
+          <span className="created">{time}</span>
+        </label>
+        <button className="icon icon-edit"></button>
+        <button className="icon icon-destroy" onClick={onDeleted}></button>
+      </div>
+    );
+  }
+}
