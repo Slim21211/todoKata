56933f180f3aaf14270df1611be59dda4922b0fd
Vladimir Davydov <v.g.davydoff@gmail.com>
2023-07-05 19:58:46 +0300

    Checked and delete #2

--- src/components/task-list.js
+++ src/components/task-list.js
@@ -2,54 +2,9 @@ import React, { Component } from "react";
 import { Task } from "./task";
 
 export class TaskList extends Component {
-  state = {
-    todoData: [
-      {
-        label: "Completed task",
-        time: "created 17 seconds ago",
-        status: null,
-        id: 1,
-      },
-      {
-        label: "Editing task",
-        time: "created 5 minutes ago",
-        status: null,
-        id: 2,
-      },
-      {
-        label: "Active task",
-        time: "created 5 minutes ago",
-        status: null,
-        id: 3,
-      },
-    ],
-  };
-  onTaskClick = (id) => {
-    this.setState(({ todoData }) => {
-      const newArr = JSON.parse(JSON.stringify(todoData));
-      newArr.map((elem) => {
-        if (elem.id === id && elem.status === null) {
-          elem.status = "completed";
-        } else if (elem.id === id && elem.status === "completed") {
-          elem.status = null;
-        }
-      });
-      return {
-        todoData: newArr,
-      };
-    });
-  };
-  onTaskDelete = (id) => {
-    this.setState(({ todoData }) => {
-      const idx = todoData.findIndex((elem) => elem.id === id);
-      const newArr = [...todoData.slice(0, idx), ...todoData.slice(idx + 1)];
-      return {
-        todoData: newArr,
-      };
-    });
-  };
   render() {
-    const elements = this.state.todoData.map((elem) => {
+    const { todo, onCheked, onDeleted } = this.props;
+    const elements = todo.todoData.map((elem) => {
       let styleName = elem.status;
       if (elem.status === "completed") {
         styleName = "completed";
@@ -67,8 +22,8 @@ export class TaskList extends Component {
           <Task
             label={elem.label}
             time={elem.time}
-            onCheked={() => this.onTaskClick(elem.id)}
-            onDeleted={() => this.onTaskDelete(elem.id)}
+            onCheked={() => onCheked(elem.id)}
+            onDeleted={() => onDeleted(elem.id)}
           />
         </li>
       );
--- src/index.js
+++ src/index.js
@@ -6,11 +6,63 @@ import { Footer } from "./components/footer";
 import "./components/index.css";
 
 class App extends Component {
+  state = {
+    todoData: [
+      {
+        label: "Completed task",
+        time: "created 17 seconds ago",
+        status: null,
+        id: 1,
+      },
+      {
+        label: "Editing task",
+        time: "created 5 minutes ago",
+        status: null,
+        id: 2,
+      },
+      {
+        label: "Active task",
+        time: "created 5 minutes ago",
+        status: null,
+        id: 3,
+      },
+    ],
+  };
+
+  onTaskClick = (id) => {
+    this.setState(({ todoData }) => {
+      todoData.map((elem) => {
+        if (elem.id === id && elem.status === null) {
+          elem.status = "completed";
+        } else if (elem.id === id && elem.status === "completed") {
+          elem.status = null;
+        }
+      });
+      return {
+        todoData: todoData,
+      };
+    });
+  };
+
+  onTaskDelete = (id) => {
+    this.setState(({ todoData }) => {
+      const idx = todoData.findIndex((elem) => elem.id === id);
+      const newArr = [...todoData.slice(0, idx), ...todoData.slice(idx + 1)];
+      return {
+        todoData: newArr,
+      };
+    });
+  };
+
   render() {
     return (
       <section className="main">
         <NewTaskForm />
-        <TaskList />
+        <TaskList
+          todo={this.state}
+          onCheked={(id) => this.onTaskClick(id)}
+          onDeleted={(id) => this.onTaskDelete(id)}
+        />
         <Footer />
       </section>
     );
